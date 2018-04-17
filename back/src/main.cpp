b121f698ac8551f7c53702bea1b6bd4ec83cc24e
Soline Ducousso <soline.ducousso@ensta-paristech.fr>
2018-04-17 16:01:41 +0200

    init.cpp

--- back/src/main.cpp
+++ back/src/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[])
 {
 
     Game game;
+    game.init();
+    
     std::vector<Pacman> pacmans = game.getPacmans();
     for (int i = 0; i < pacmans.size(); i++)
     {
