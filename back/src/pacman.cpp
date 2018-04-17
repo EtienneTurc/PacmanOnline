620f84e53fd5f2c061618c41de9ace02bcce364a
Soline Ducousso <soline.ducousso@ensta-paristech.fr>
2018-04-17 14:49:28 +0200

    commit before merge

--- back/src/pacman.cpp
+++ back/src/pacman.cpp
@@ -1,8 +1,6 @@
 #include "pacman.h"
 
-Pacman::Pacman(uint8_t x_position, uint8_t y_position, char direction, uint8_t speed, uint8_t subsection) : Entity(x_position, y_position, direction, speed, subsection) {
-
-}
+Pacman::Pacman(uint8_t x_position, uint8_t y_position, char direction, uint8_t speed, uint8_t subsection) : Entity(x_position, y_position, direction, speed, subsection) {}
 
 void Pacman::eat(uint8_t** grid) {
     grid[_x_position][_y_position] = 1; 
