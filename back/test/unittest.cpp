5ea5dd55b9b04c861c6886b230f9efb8066a75ff
Soline Ducousso <soline.ducousso@ensta-paristech.fr>
2018-05-18 14:41:43 +0200

    a little unit test for the grid

--- back/test/unittest.cpp
+++ back/test/unittest.cpp
@@ -4,8 +4,13 @@
 namespace {
 
 	TEST(GridTest, getCell) {
-		Grid grid();
-		EXPECT_EQ(0, 0);
+		Grid* grid = new Grid;
+		EXPECT_EQ(0, grid->getCell(0, 0));
+	}
+	TEST(GridTest, setCell) {
+		Grid* grid = new Grid;
+		grid->setCell(0, 0, 3);
+		EXPECT_EQ(3, grid->getCell(0, 0));
 	}
 
 }
